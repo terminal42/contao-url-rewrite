0d9d3d395fe72d8d609b15697e8beecab73225b9
Kamil Kuzminski <kamil.kuzminski@codefog.pl>
2021-07-22 12:02:07 +0200

    Add QrCodeGenerator unit tests

---
+++ tests/QrCodeGeneratorTest.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Terminal42\UrlRewriteBundle\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Routing\Exception\MissingMandatoryParametersException;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\Router;
+use Symfony\Component\Routing\RouterInterface;
+use Terminal42\UrlRewriteBundle\ConfigProvider\DatabaseConfigProvider;
+use Terminal42\UrlRewriteBundle\QrCodeGenerator;
+use Terminal42\UrlRewriteBundle\Routing\UrlRewriteLoader;
+
+class QrCodeGeneratorTest extends TestCase
+{
+    /**
+     * @var QrCodeGenerator
+     */
+    private $qrCodeGenerator;
+
+    /**
+     * @var RouterInterface
+     */
+    private $router;
+
+    protected function setUp(): void
+    {
+        $this->router = $this->createMock(Router::class);
+        $this->qrCodeGenerator = new QrCodeGenerator($this->router);
+    }
+
+    public function testInstantiation()
+    {
+        $this->assertInstanceOf(QrCodeGenerator::class, $this->qrCodeGenerator);
+    }
+
+    public function testValidate()
+    {
+        $this->assertTrue($this->qrCodeGenerator->validate(['requestPath' => 'foo/bar', 'inactive' => false]));
+
+        $this->assertFalse($this->qrCodeGenerator->validate(['requestPath' => 'foo/bar', 'inactive' => true]));
+        $this->assertFalse($this->qrCodeGenerator->validate(['requestPath' => '', 'inactive' => true]));
+        $this->assertFalse($this->qrCodeGenerator->validate(['requestPath' => '', 'inactive' => false]));
+    }
+
+    public function testGenerateImage()
+    {
+        $image = $this->qrCodeGenerator->generateImage('https://domain.tld/foo/bar?test=1');
+
+        $this->assertSame(file_get_contents(__DIR__ . '/Fixtures/qr-code.svg'), $image);
+    }
+
+    public function testGenerateUrl()
+    {
+        $routeIncorrect = new Route('foo/baz');
+
+        $routeCorrect1 = new Route('foo/bar');
+        $routeCorrect1->setHost('domain.tld');
+        $routeCorrect1->setDefault(UrlRewriteLoader::ATTRIBUTE_NAME, DatabaseConfigProvider::class . ':123');
+
+        $routeCorrect2 = new Route('foo/bar');
+        $routeCorrect2->setDefault(UrlRewriteLoader::ATTRIBUTE_NAME, DatabaseConfigProvider::class . ':456');
+
+        $this->router->method('getRouteCollection')->willReturn([666 => $routeIncorrect, 123 => $routeCorrect1, 456 => $routeCorrect2]);
+        $this->router->method('getContext')->willReturn(new RequestContext);
+        $this->router->method('generate')->willReturn('https://domain.tld/foo/bar');
+
+        $this->assertEquals('https://domain.tld/foo/bar', $this->qrCodeGenerator->generateUrl(['id' => 123], ['host' => 'domain.tld', 'scheme' => 'https']));
+        $this->assertEquals('https://domain.tld/foo/bar', $this->qrCodeGenerator->generateUrl(['id' => 456], ['host' => 'domain.tld']));
+    }
+
+    public function testGenerateUrlMissingMandatoryParametersException()
+    {
+        $this->expectException(MissingMandatoryParametersException::class);
+        $this->expectExceptionMessage('The parameter "host" is mandatory');
+
+        $this->qrCodeGenerator->generateUrl([]);
+    }
+
+    public function testGenerateUrlRuntimeException()
+    {
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionMessage('Unable to determine route ID for rewrite ID 456');
+
+        $this->router->method('getRouteCollection')->willReturn([]);
+        $this->qrCodeGenerator->generateUrl(['id' => 456], ['host' => 'domain.tld']);
+    }
+}
