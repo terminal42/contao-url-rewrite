fb73392b12216abc6229290a878f3a7de429cb91
Yanick Witschi <yanick.witschi@terminal42.ch>
2025-07-07 16:14:09 +0200

    CS

--- src/Controller/QrCodeController.php
+++ src/Controller/QrCodeController.php
@@ -16,7 +16,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\UriSigner;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Routing\Exception\InvalidParameterException;
 use Symfony\Component\Routing\Exception\MissingMandatoryParametersException;
 use Symfony\Component\Routing\RouterInterface;
